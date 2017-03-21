c4a993080dc42d4880ed4fb160c10680c69cfb33
stevenkur <steven_kurkur@yahoo.com>
2017-03-21 13:16:58 +0700

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+//    public function __construct() 
+//    { 
+//        $this->middleware('auth'); 
+//    } 
+        
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+}
