65992c3ff4ab7980d6156d3202243512a3d94b54
stevenkur <steven_kurkur@yahoo.com>
2017-03-21 23:27:07 +0700

    home admin

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -1,35 +1,34 @@
-
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>EZ-MED | Dashboard</title>
+  <title>Monitoring Workshop | Dashboard</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
-  <link rel="stylesheet" href="admin/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="adminlte/bootstrap/css/bootstrap.min.css">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="admin/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="adminlte/dist/css/AdminLTE.min.css">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="admin/dist/css/skins/_all-skins.min.css">
+  <link rel="stylesheet" href="adminlte/dist/css/skins/_all-skins.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="admin/plugins/iCheck/flat/blue.css">
+  <link rel="stylesheet" href="adminlte/plugins/iCheck/flat/blue.css">
   <!-- Morris chart -->
-  <link rel="stylesheet" href="admin/plugins/morris/morris.css">
+  <link rel="stylesheet" href="adminlte/plugins/morris/morris.css">
   <!-- jvectormap -->
-  <link rel="stylesheet" href="admin/plugins/jvectormap/jquery-jvectormap-1.2.2.css">
+  <link rel="stylesheet" href="adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.css">
   <!-- Date Picker -->
-  <link rel="stylesheet" href="admin/plugins/datepicker/datepicker3.css">
+  <link rel="stylesheet" href="adminlte/plugins/datepicker/datepicker3.css">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="admin/plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="adminlte/plugins/daterangepicker/daterangepicker.css">
   <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href="admin/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+  <link rel="stylesheet" href="adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -47,9 +46,9 @@
     <!-- Logo -->
     <a href="{{ route('admins.index') }}" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>EZM</span>
+      <span class="logo-mini"><b>MWS</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Admin </b>EZ-MED</span>
+      <span class="logo-lg"><b>Administrator </b></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
@@ -74,7 +73,7 @@
                   <li><!-- start message -->
                     <a href="#">
                       <div class="pull-left">
-                        <img src="admin/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                        <img src="adminlte/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
                       </div>
                       <h4>
                         Support Team
@@ -87,7 +86,7 @@
                   <li>
                     <a href="#">
                       <div class="pull-left">
-                        <img src="admin/dist/img/user3-128x128.jpg" class="img-circle" alt="User Image">
+                        <img src="adminlte/dist/img/user3-128x128.jpg" class="img-circle" alt="User Image">
                       </div>
                       <h4>
                         AdminLTE Design Team
@@ -99,7 +98,7 @@
                   <li>
                     <a href="#">
                       <div class="pull-left">
-                        <img src="admin/dist/img/user4-128x128.jpg" class="img-circle" alt="User Image">
+                        <img src="adminlte/dist/img/user4-128x128.jpg" class="img-circle" alt="User Image">
                       </div>
                       <h4>
                         Developers
@@ -111,7 +110,7 @@
                   <li>
                     <a href="#">
                       <div class="pull-left">
-                        <img src="admin/dist/img/user3-128x128.jpg" class="img-circle" alt="User Image">
+                        <img src="adminlte/dist/img/user3-128x128.jpg" class="img-circle" alt="User Image">
                       </div>
                       <h4>
                         Sales Department
@@ -123,7 +122,7 @@
                   <li>
                     <a href="#">
                       <div class="pull-left">
-                        <img src="admin/dist/img/user4-128x128.jpg" class="img-circle" alt="User Image">
+                        <img src="adminlte/dist/img/user4-128x128.jpg" class="img-circle" alt="User Image">
                       </div>
                       <h4>
                         Reviewers
@@ -256,13 +255,13 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="admin/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <img src="adminlte/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
               <span class="hidden-xs">Alexander Pierce</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
-                <img src="admin/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <img src="adminlte/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
                 <p>
                   Alexander Pierce - Web Developer
@@ -310,7 +309,7 @@
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="admin/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+          <img src="adminlte/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p>Alexander Pierce</p>
@@ -329,6 +328,117 @@
       </form>
       <!-- /.search form -->
     
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu">
+        <li class="header">MAIN NAVIGATION</li>
+        <li class="active treeview">
+          <a href="{{ route('admins.index') }}">
+            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+          </a>
+        </li>
+          
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-medkit"></i> <!-- logo belum diganti -->
+            <span>Create Ship Data</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href=""><i class="fa fa-circle-o"></i> Register Project of Ship</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Input Material List</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Input Assembly Part List</a></li>
+          </ul>
+        </li>
+         
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-medkit"></i> <!-- logo belum diganti -->
+            <span>Register Application User</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href=""><i class="fa fa-circle-o"></i> Register New User</a></li>
+            <li><a href=""><i class="fa fa-circle-o"></i> Recapitulation User</a></li>
+          </ul>
+        </li>
+
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-medkit"></i> <!-- logo belum diganti -->
+            <span>Input Machine</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href=""><i class="fa fa-circle-o"></i> Input New Machine</a></li>
+            <li><a href=""><i class="fa fa-circle-o"></i> Recap Machine</a></li>
+          </ul>
+        </li>
+
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-medkit"></i> <!-- logo belum diganti -->
+            <span>Input Worker</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href=""><i class="fa fa-circle-o"></i> Input New Worker in Workshop</a></li>
+            <li><a href=""><i class="fa fa-circle-o"></i> Recap Worker</a></li>
+          </ul>
+        </li>
+
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-medkit"></i> <!-- logo belum diganti -->
+            <span>Monitoring Production Workshop</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href=""><i class="fa fa-circle-o"></i> SSH</a></li>
+            <li><a href=""><i class="fa fa-circle-o"></i> Fabrication</a></li>
+            <li><a href=""><i class="fa fa-circle-o"></i> Sub Assembly</a></li>
+            <li><a href=""><i class="fa fa-circle-o"></i> Assembly</a></li>
+            <li><a href=""><i class="fa fa-circle-o"></i> BBS</a></li>
+            <li><a href=""><i class="fa fa-circle-o"></i> Erection Process</a></li>
+          </ul>
+        </li>
+          
+        <li>
+          <a href="adminlte/pages/calendar.html">
+            <i class="fa fa-calendar"></i> <span>Calendar</span>
+            <span class="pull-right-container">
+              <small class="label pull-right bg-red">3</small>
+              <small class="label pull-right bg-blue">17</small>
+            </span>
+          </a>
+        </li>
+          
+        <li>
+          <a href="adminlte/pages/mailbox/mailbox.html">
+            <i class="fa fa-envelope"></i> <span>Mailbox</span>
+            <span class="pull-right-container">
+              <small class="label pull-right bg-yellow">12</small>
+              <small class="label pull-right bg-green">16</small>
+              <small class="label pull-right bg-red">5</small>
+            </span>
+          </a>
+        </li>
+        
+        <li><a href="adminlte/documentation/index.html"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
+
     @yield('content')
     
     <footer class="main-footer">
@@ -537,7 +647,7 @@
 
     
     <!-- jQuery 2.2.3 -->
-<script src="admin/plugins/jQuery/jquery-2.2.3.min.js"></script>
+<script src="adminlte/plugins/jQuery/jquery-2.2.3.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
 <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
@@ -545,35 +655,35 @@
   $.widget.bridge('uibutton', $.ui.button);
 </script>
 <!-- Bootstrap 3.3.6 -->
-<script src="admin/bootstrap/js/bootstrap.min.js"></script>
+<script src="adminlte/bootstrap/js/bootstrap.min.js"></script>
 <!-- Morris.js charts -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-<script src="admin/plugins/morris/morris.min.js"></script>
+<script src="adminlte/plugins/morris/morris.min.js"></script>
 <!-- ChartJS 1.0.1 -->
-<script src="admin/plugins/chartjs/Chart.min.js"></script>
+<script src="adminlte/plugins/chartjs/Chart.min.js"></script>
 <!-- Sparkline -->
-<script src="admin/plugins/sparkline/jquery.sparkline.min.js"></script>
+<script src="adminlte/plugins/sparkline/jquery.sparkline.min.js"></script>
 <!-- jvectormap -->
-<script src="admin/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="admin/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<script src="adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="adminlte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
 <!-- jQuery Knob Chart -->
-<script src="admin/plugins/knob/jquery.knob.js"></script>
+<script src="adminlte/plugins/knob/jquery.knob.js"></script>
 <!-- daterangepicker -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
-<script src="admin/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="adminlte/plugins/daterangepicker/daterangepicker.js"></script>
 <!-- datepicker -->
-<script src="admin/plugins/datepicker/bootstrap-datepicker.js"></script>
+<script src="adminlte/plugins/datepicker/bootstrap-datepicker.js"></script>
 <!-- Bootstrap WYSIHTML5 -->
-<script src="admin/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+<script src="adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
 <!-- Slimscroll -->
-<script src="admin/plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<script src="adminlte/plugins/slimScroll/jquery.slimscroll.min.js"></script>
 <!-- FastClick -->
-<script src="admin/plugins/fastclick/fastclick.js"></script>
+<script src="adminlte/plugins/fastclick/fastclick.js"></script>
 <!-- AdminLTE App -->
-<script src="admin/dist/js/app.min.js"></script>
+<script src="adminlte/dist/js/app.min.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="admin/dist/js/pages/dashboard.js"></script>
+<script src="adminlte/dist/js/pages/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="admin/dist/js/demo.js"></script>
+<script src="adminlte/dist/js/demo.js"></script>
 </body>
 </html>
