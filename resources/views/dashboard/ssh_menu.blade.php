47028dda5e2cdee9103c983a3708cb980b42425b
stevenkur <steven_kurkur@yahoo.com>
2017-03-30 14:02:47 +0700

    add admin ssh

--- resources/views/dashboard/ssh_menu.blade.php
+++ resources/views/dashboard/ssh_menu.blade.php
@@ -31,7 +31,7 @@
                   <select class="form-control">
                     <option id="#">--Recap Activity--</option>
                     <option id="1">Recap Material Coming</option>
-                    <option id="2">Recap Material Process (Blasting&Shop Primer)</option>
+                    <option id="2">Recap Material Process (Blasting & Shop Primer)</option>
                   </select>
                 </div>
                
@@ -43,24 +43,252 @@
               </div>
             </form>
           </div>
-
           </div>
+
+        <div class="col-md-12">
+          <div class="box box-primary">
+            
+          <div class="box-body">
+              <table id="material" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>Name of Ship</th>
+                  <th>Many of Material</th>
+                  <th>Many of Material Come</th>
+                  <th>Progress</th>
+                  <th>View Detail</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td>Name of Ship</td>
+                  <td>Many of Material</td>
+                  <td>Many of Material Come</td>
+                  <td>Progress</td>
+                  <td>View Detail</td>
+                </tr>
+                <tr>
+                  <td>Name of Ship</td>
+                  <td>Many of Material</td>
+                  <td>Many of Material Come</td>
+                  <td>Progress</td>
+                  <td>View Detail</td>
+                </tr>
+                <tr>
+                  <td>Name of Ship</td>
+                  <td>Many of Material</td>
+                  <td>Many of Material Come</td>
+                  <td>Progress</td>
+                  <td>View Detail</td>
+                </tr>
+                <tr>
+                  <td>Name of Ship</td>
+                  <td>Many of Material</td>
+                  <td>Many of Material Come</td>
+                  <td>Progress</td>
+                  <td>View Detail</td>
+                </tr>
+                </tbody>
+                <tfoot>
+                <tr>
+                  <th>Name of Ship</th>
+                  <th>Many of Material</th>
+                  <th>Many of Material Come</th>
+                  <th>Progress</th>
+                  <th>View Detail</th>
+                </tr>
+                </tfoot>
+              </table>
+             </div>
+           </div>
+        </div>
+
+
+        <div class="col-md-12">
+        <div class="box box-primary">
+            
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form">
+              <div class="box-body">
+              <label for="inputActivity">Select Project of Ship:</label>
+                <div class="form-group">
+                  <select class="form-control">
+                    <option id="#">-- Ship Project List --</option>
+                    <option id="1">Project 1</option>
+                    <option id="2">Project 2</option>
+                    <option id="3">Project 3</option>
+                    <option id="4">Project 4</option>
+                  </select>
+                </div>
+               
+              </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Choose</button>
+              </div>
+            </form>
+            </div>
+          </div>
+
+          <div class="col-md-12">
+          <div class="box box-primary">
+          <h1>Plate</h1>  
+          <div class="box-body">
+              <table id="plate" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>ID Material</th>
+                  <th>Dimension</th>
+                  <th>Quantity</th>
+                  <th>Weight</th>
+                  <th>Date of Coming</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td>ID Material</td>
+                  <td>Dimension</td>
+                  <td>Quantity</td>
+                  <td>Weight</td>
+                  <td>Date of Coming</td>
+                </tr>
+                <tr>
+                  <td>ID Material</td>
+                  <td>Dimension</td>
+                  <td>Quantity</td>
+                  <td>Weight</td>
+                  <td>Date of Coming</td>
+                </tr>
+                <tr>
+                  <td>ID Material</td>
+                  <td>Dimension</td>
+                  <td>Quantity</td>
+                  <td>Weight</td>
+                  <td>Date of Coming</td>
+                </tr>
+                <tr>
+                  <td>ID Material</td>
+                  <td>Dimension</td>
+                  <td>Quantity</td>
+                  <td>Weight</td>
+                  <td>Date of Coming</td>
+                </tr>
+                </tbody>
+                <tfoot>
+                <tr>
+                  <th>ID Material</th>
+                  <th>Dimension</th>
+                  <th>Quantity</th>
+                  <th>Weight</th>
+                  <th>Date of Coming</th>
+                </tr>
+                </tfoot>
+              </table>
+             </div>
+             <h1>Profil</h1>  
+              <div class="box-body">
+              <table id="profil" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>ID Material</th>
+                  <th>Dimension</th>
+                  <th>Quantity</th>
+                  <th>Weight</th>
+                  <th>Date of Coming</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td>ID Material</td>
+                  <td>Dimension</td>
+                  <td>Quantity</td>
+                  <td>Weight</td>
+                  <td>Date of Coming</td>
+                </tr>
+                <tr>
+                  <td>ID Material</td>
+                  <td>Dimension</td>
+                  <td>Quantity</td>
+                  <td>Weight</td>
+                  <td>Date of Coming</td>
+                </tr>
+                <tr>
+                  <td>ID Material</td>
+                  <td>Dimension</td>
+                  <td>Quantity</td>
+                  <td>Weight</td>
+                  <td>Date of Coming</td>
+                </tr>
+                <tr>
+                  <td>ID Material</td>
+                  <td>Dimension</td>
+                  <td>Quantity</td>
+                  <td>Weight</td>
+                  <td>Date of Coming</td>
+                </tr>
+                </tbody>
+                <tfoot>
+                <tr>
+                  <th>ID Material</th>
+                  <th>Dimension</th>
+                  <th>Quantity</th>
+                  <th>Weight</th>
+                  <th>Date of Coming</th>
+                </tr>
+                </tfoot>
+              </table>
+             </div>
+           </div>
+        </div>
 
       </div>
     </section>
   </div>
 
 @stop
-
 <!-- jQuery 2.2.3 -->
 <script src="adminlte/plugins/jQuery/jquery-2.2.3.min.js"></script>
 <!-- Bootstrap 3.3.6 -->
 <script src="adminlte/bootstrap/js/bootstrap.min.js"></script>
-<!-- ChartJS 1.0.1 -->
-<script src="adminlte/plugins/chartjs/Chart.min.js"></script>
+<!-- DataTables -->
+<script src="adminlte/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="adminlte/plugins/datatables/dataTables.bootstrap.min.js"></script>
+<!-- SlimScroll -->
+<script src="adminlte/plugins/slimScroll/jquery.slimscroll.min.js"></script>
 <!-- FastClick -->
 <script src="adminlte/plugins/fastclick/fastclick.js"></script>
 <!-- AdminLTE App -->
 <script src="adminlte/dist/js/app.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="admin/dist/js/demo.js"></script>
+<script src="adminlte/dist/js/demo.js"></script>
+<!-- page script -->
+<script>
+$(function() {
+    $('#material').DataTable({
+          "paging": true,
+          "lengthChange": true,
+          "searching": true,
+          "ordering": true,
+          "info": true,
+          "autoWidth": true
+    });
+    $('#plate').DataTable({
+          "paging": true,
+          "lengthChange": true,
+          "searching": true,
+          "ordering": true,
+          "info": true,
+          "autoWidth": true
+    });
+    $('#profil').DataTable({
+          "paging": true,
+          "lengthChange": true,
+          "searching": true,
+          "ordering": true,
+          "info": true,
+          "autoWidth": true
+    });
+  });
+</script>
