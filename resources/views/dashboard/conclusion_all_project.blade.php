eb151b6c0f844c361deddb4113c49eb3cc892fbe
stevenkur <steven_kurkur@yahoo.com>
2017-05-05 12:36:45 +0700

    add tambahan view admin, belum di route

---
+++ resources/views/dashboard/conclusion_all_project.blade.php
@@ -0,0 +1,85 @@
+@extends('layouts.backend')
+
+@section('content')
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Total Ship Progress
+      </h1>
+      <ol class="breadcrumb">
+        <li><i class="fa fa-dashboard"></i> Home</li>
+        <li class="active">Total Ship Progress</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+          <!-- /.box-header -->
+          <!-- form start -->  
+          <div class="col-md-12">
+          <div class="box box-primary">
+          <div class="box-body">
+              <label for="viewPanel">Panel List</label>
+              <table id="panel" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>SHIP NAME</th>
+                  <th>PROGRESS</th>
+                  <th>START PROJECT</th>
+                  <th>TARGET FINISH</th>
+                  <th>LAST DATE WORKED</th>
+                </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+                <tfoot>
+                <tr>
+                  <th>SHIP NAME</th>
+                  <th>PROGRESS</th>
+                  <th>START PROJECT</th>
+                  <th>TARGET FINISH</th>
+                  <th>LAST DATE WORKED</th>
+                </tr>
+                </tfoot>
+              </table>
+            </div>
+            </div>
+          </div>
+          </div>
+    </section>
+  </div>
+
+@stop
+
+<!-- jQuery 2.2.3 -->
+<script src="adminlte/plugins/jQuery/jquery-2.2.3.min.js"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src="adminlte/bootstrap/js/bootstrap.min.js"></script>
+<!-- DataTables -->
+<script src="adminlte/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="adminlte/plugins/datatables/dataTables.bootstrap.min.js"></script>
+<!-- SlimScroll -->
+<script src="adminlte/plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="adminlte/plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="adminlte/dist/js/app.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="adminlte/dist/js/demo.js"></script>
+<!-- page script -->
+<script>
+$(function() {
+    $('#panel').DataTable({
+          "paging": true,
+          "lengthChange": true,
+          "searching": true,
+          "ordering": true,
+          "info": true,
+          "autoWidth": true
+    });
+  });
+</script>
