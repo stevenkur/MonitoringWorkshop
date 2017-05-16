2122b87b0596f86404f9630fddbf3a4147f9e9e9
prasetyon <prstyngrhd@gmail.com>
2017-05-16 15:28:41 +0700

    DB Fix

--- database/migrations/2017_05_16_071343_create_b_b_s_table.php
+++ database/migrations/2017_05_16_071343_create_b_b_s_table.php
@@ -14,12 +14,18 @@ class CreateBBSTable extends Migration
     public function up()
     {
         Schema::create('b_b_s', function (Blueprint $table) {
-            $table->string('ROOM', 25)->index();
+            $table->string('ID_BBS', 25)->index();
+            $table->string('ROOM', 25);
+            $table->integer('ID_PROJECT');
+            $table->string('PROJECT_NAME', 55);
+            $table->integer('ID_BLOCK', 25);
+            $table->string('BLOCK_NAME', 25);
             $table->string('SIDE', 25);
             $table->string('FRAME', 25);
             $table->string('DECK', 25);
             $table->integer('AREA');
             $table->integer('TOTAL_LAYER');
+            $table->integer('FINISHED_LAYER')->default(0);
             $table->string('PAINT_TYPE', 25);
             $table->integer('PAINT_NEEDS');
             $table->timestamps();
