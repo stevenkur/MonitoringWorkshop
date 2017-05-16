9e53611b933e9e07eb5b42f4bc3a005c8ec52eb3
stevenkur <steven_kurkur@yahoo.com>
2017-05-16 15:14:32 +0700

    model bbs

--- database/migrations/2017_05_16_071343_create_b_b_s_table.php
+++ database/migrations/2017_05_16_071343_create_b_b_s_table.php
@@ -14,7 +14,14 @@ class CreateBBSTable extends Migration
     public function up()
     {
         Schema::create('b_b_s', function (Blueprint $table) {
-            $table->increments('id');
+            $table->string('ROOM', 25)->index();
+            $table->string('SIDE', 25);
+            $table->string('FRAME', 25);
+            $table->string('DECK', 25);
+            $table->integer('AREA');
+            $table->integer('TOTAL_LAYER');
+            $table->string('PAINT_TYPE', 25);
+            $table->integer('PAINT_NEEDS');
             $table->timestamps();
         });
     }
--- resources/views/user/bbs_calculate_paint_needs.blade.php
+++ resources/views/user/bbs_calculate_paint_needs.blade.php
@@ -18,72 +18,16 @@
     <!-- Main content -->
     <section class="content">
       <div class="row">
-
-            <section class="col-lg-6">
-            <div class="box box-primary">
-            
-            <!-- /.box-header -->
-            <!-- form start -->
-            <form role="form" name="ShipProject">
-              <div class="box-body">
-              <label for="inputActivity">Select Project of Ship:</label>
-                <div class="form-group">
-                  <select class="form-control" name="project">
-                    <option value="#">-- Ship Project List --</option>
-                    <?php $i=1;?>
-                    @foreach($ship as $data)
-                        <?php $shipData[$i] = $data; $i++;?>
-                        <option value="{{$data->ID}}">{{$data->PROJECT_NAME}}</option>
-                    @endforeach
-                  </select>
-                </div>
-               
-              </div>
-              <!-- /.box-body -->
-
-              <div class="box-footer">
-                <button type="submit" class="btn btn-primary">Choose</button>
-              </div>
-            </form>
-            </div>
-            </section>
-
-            <section class="col-lg-6">
-            <div class="box box-primary">
-            
-            <!-- /.box-header -->
-            <!-- form start -->
-            <form role="form" name="ShipBlock">
-              <div class="box-body">
-              <label for="inputActivity">Select BLock:</label>
-                <div class="form-group">
-                  <select class="form-control" name="block">
-                    <option value="#">-- Block List --</option>
-                    <?php $i=1;?>
-                    @foreach($block as $datas)
-                        <?php $blockData[$i] = $datas; $i++;?>
-                        <option value="{{$datas->ID}}">{{$datas->NAME}}</option>
-                    @endforeach
-                  </select>
-                </div>
-               
-              </div>
-              <!-- /.box-body -->
-
-              <div class="box-footer">
-                <button type="submit" class="btn btn-primary">Choose</button>
-              </div>
-            </form>
-            </div>
-            </section>
-
-        <div class="col-md-7">
-        <div class="box">
+    
+        <div class="col-md-12">
+        <div class="box box-primary">
             <!-- /.box-header -->
             <div class="box-body">
               <table id="machine" class="table table-bordered table-striped">
                 <thead>
                 <tr>
+                  <th>Ship Project</th>
+                  <th>Block</th>
                   <th>Room</th>
                   <th>Side</th>
                   <th>Frame</th>
@@ -96,6 +40,8 @@
                 </thead>
                 <tbody>
                 <tr>
+                  <td>Ship Project</td>
+                  <td>Block</td>
                   <td>Room</td>
                   <td>Side</td>
                   <td>Frame</td>
@@ -106,6 +52,8 @@
                   <td>Hitung Rumus</td>
                 </tr>
                 <tr>
+                  <td>Ship Project</td>
+                  <td>Block</td>
                   <td>Room</td>
                   <td>Side</td>
                   <td>Frame</td>
@@ -116,6 +64,8 @@
                   <td>Hitung Rumus</td>
                 </tr>
                 <tr>
+                  <td>Ship Project</td>
+                  <td>Block</td>
                   <td>Room</td>
                   <td>Side</td>
                   <td>Frame</td>
@@ -126,6 +76,8 @@
                   <td>Hitung Rumus</td>
                 </tr>
                 <tr>
+                  <td>Ship Project</td>
+                  <td>Block</td>
                   <td>Room</td>
                   <td>Side</td>
                   <td>Frame</td>
@@ -138,64 +90,153 @@
                 </tbody>
                 <tfoot>
                 <tr>
+                  <th>Ship Project</th>
+                  <th>Block</th>
                   <th>Room</th>
                   <th>Side</th>
                   <th>Frame</th>
                   <th>Deck</th>
-                  <th>Area(m2)</th>
-                  <td>Layer</td>
-                  <td>Cat</td>
-                  <td>Hitung Rumus</td>
+                  <th>Area(m<sup>2</sup>)</th>
+                  <th>Total Layer</th>
+                  <th>Paint Type</th>
+                  <th>Paint Needs</th>
                 </tr>
                 </tfoot>
               </table>
             </div>
             <!-- /.box-body -->
-          </div>
+        </div>
         </div>
 
-        <section class="col-lg-5">
+        <section class="col-lg-12">
         <div class="box box-primary">
+        <div class="box-body">
+        <h3>Insert Rooms</h3>
         <form role="form" name="BBSPaint">
                 <br>
                 <div class="form-group">
-                  <label class="col-lg-4"> Room: </label>
-                  <input type="text" id="room" placeholder="">
+                  <label class="col-lg-2"> Ship Project </label>
+                  <label class="col-lg-1"> : </label>
+                  <div class="col-lg-6">
+                  <select class="form-control" name="project">
+                    <option value="#">-- Ship Project List --</option>
+                    <?php $i=1;?>
+                    @foreach($ship as $data)
+                        <?php $shipData[$i] = $data; $i++;?>
+                        <option value="{{$data->ID}}">{{$data->PROJECT_NAME}}</option>
+                    @endforeach
+                  </select>
+                  </div>
                 </div>
+                <br>
+                <br>
                 <div class="form-group">
-                  <label class="col-lg-4"> Area: </label>
-                  <input type="text" id="area" placeholder="">
-                  <label> m2 </label>
+                  <label class="col-lg-2"> Block </label>
+                  <label class="col-lg-1"> : </label>
+                  <div class="col-lg-6">
+                  <select class="form-control" name="block">
+                    <option value="#">-- Block List --</option>
+                    <?php $i=1;?>
+                    @foreach($block as $datas)
+                        <?php $blockData[$i] = $datas; $i++;?>
+                        <option value="{{$datas->ID}}">{{$datas->NAME}}</option>
+                    @endforeach
+                  </select>
+                  </div>
                 </div>
+                <br>
+                <br>
                 <div class="form-group">
-                  <label class="col-lg-4"> Dry Film Thickness: </label>
-                  <input type="text" id="dryfilm" placeholder="">
+                  <label class="col-lg-2"> Room </label>
+                  <label class="col-lg-1"> : </label>
+                  <div class="col-lg-6">
+                  <input type="text" id="room" placeholder="" style="width:550px">
+                  </div>
+                </div>
+                <br>
+                <div class="form-group">
+                  <label class="col-lg-2"> Side </label>
+                  <label class="col-lg-1"> : </label>
+                  <div class="col-lg-6">
+                  <input type="text" id="side" placeholder="" style="width:550px">
+                  </div>
+                </div>
+                <br>
+                <div class="form-group">
+                  <label class="col-lg-2"> Frame </label>
+                  <label class="col-lg-1"> : </label>
+                  <div class="col-lg-6">
+                  <input type="text" id="frame" placeholder="" style="width:550px">
+                  </div>
+                </div>
+                <br>
+                <div class="form-group">
+                  <label class="col-lg-2"> Deck </label>
+                  <label class="col-lg-1"> : </label>
+                  <div class="col-lg-6">
+                  <input type="text" id="deck" placeholder="" style="width:550px">
+                  </div>
+                </div>
+                <br>
+                <div class="form-group">
+                  <label class="col-lg-2"> Area </label>
+                  <label class="col-lg-1"> : </label>
+                  <div class="col-lg-6">
+                  <input type="text" id="area" placeholder="" style="width:550px">
+                  <label> m<sup>2</sup> </label>
+                  </div>
+                </div>
+                <br>
+                <div class="form-group">
+                  <label class="col-lg-2"> Dry Film Thickness </label>
+                  <label class="col-lg-1"> : </label>
+                  <div class="col-lg-6">
+                  <input type="text" id="dryfilm" placeholder="" style="width:550px">
                   <label> mikron </label>
+                  </div>
                 </div>
+                <br>
                 <div class="form-group">
-                  <label class="col-lg-4"> Volume Solid: </label>
-                  <input type="text" id="volumesolid" placeholder="">
+                  <label class="col-lg-2"> Volume Solid </label>
+                  <label class="col-lg-1"> : </label>
+                  <div class="col-lg-6">
+                  <input type="text" id="volumesolid" placeholder="" style="width:550px">
                   <label> % </label>
+                  </div>
                 </div>
+                <br>
                 <div class="form-group">
-                  <label class="col-lg-4"> Loss Factor: </label>
-                  <input type="text" id="lossfactor" placeholder="">
+                  <label class="col-lg-2"> Loss Factor </label>
+                  <label class="col-lg-1"> : </label>
+                  <div class="col-lg-6">
+                  <input type="text" id="lossfactor" placeholder="" style="width:550px">
                   <label> % </label>
+                  </div>
                 </div>
+                <br>
                 <div class="form-group">
-                  <label class="col-lg-4"> Total Layer: </label>
-                  <input type="text" id="layer" placeholder="">
+                  <label class="col-lg-2"> Total Layer </label>
+                  <label class="col-lg-1"> : </label>
+                  <div class="col-lg-6">
+                  <input type="text" id="layer" placeholder="" style="width:550px">
+                  </div>
                 </div>
+                <br>
                 <div class="form-group">
-                  <label class="col-lg-4"> Paint Type: </label>
-                  <input type="text" id="painttype" placeholder="">
+                  <label class="col-lg-2"> Paint Type </label>
+                  <label class="col-lg-1"> : </label>
+                  <div class="col-lg-6">
+                  <input type="text" id="painttype" placeholder="" style="width:550px">
+                  </div>
                 </div>
+                <br>
 
             <div class="box-footer" align="right">
               <button type="reset" class="btn btn-primary">Reset</button>
               <button type="submit" class="btn btn-primary">Input</button>
             </div>
         </form>
+        </div>
         </div>
         </section>
 
