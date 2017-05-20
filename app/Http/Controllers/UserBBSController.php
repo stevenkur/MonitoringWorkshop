d7e06b2191b1fc5978312c1ab467a25da6873f31
prasetyon <prstyngrhd@gmail.com>
2017-05-20 22:52:02 +0700

    ndaiso

--- app/Http/Controllers/UserBBSController.php
+++ app/Http/Controllers/UserBBSController.php
@@ -32,28 +32,38 @@ class UserBBSController extends Controller
      */
     public function bbs_add_rooms(Request $request)
     {
-        //
+        dd(Input::all());
+        $input = Input::all();
+        
+        $project = explode('|', $input['project']);
+        $projectId = $project[0];
+        $projectName = $project[1];
+        
+        $block = explode('|', $input['block']);
+        $blockId = $block[0];
+        $blockName = $block[1];
+        
         $room = new Room();
-        $room->ID_PROJECT = $request->projectid;
-        $room->PROJECT_NAME = $request->project;   
-        $room->ID_BLOCK = $request->blockid; 
-        $room->BLOCK_NAME = $request->block;       
-        $room->ROOM = $request->room;        
-        $room->SIDE = $request->side;       
-        $room->FRAME = $request->frame;  
-        $room->DECK = $request->deck; 
-        $room->AREA = $request->area; 
-        $room->TOTAL_LAYER = $request->layer; 
-        $room->PAINT_TYPE = $request->painttype;
-        $area = $request->area;
-        $dft = $request->dft;
-        $vs = $request->vs;
-        $cb = $request->lf;
+        $room->ID_PROJECT = $projectId;
+        $room->PROJECT_NAME = $projectName;   
+        $room->ID_BLOCK = $blockId; 
+        $room->BLOCK_NAME = $blockName;       
+        $room->ROOM = $input['room'];        
+        $room->SIDE = $input['side'];       
+        $room->FRAME = $input['frame'];  
+        $room->DECK = $input['deck']; 
+        $room->AREA = $input['area']; 
+        $room->TOTAL_LAYER = $input['layer']; 
+        $room->PAINT_TYPE = $input['painttype'];
+        $area = $input['area'];
+        $dft = $input['dft'];
+        $vs = $input['vs'];
+        $cb = $input['lf'];
         $hasil = $area * $dft * $cb / $vs;   
         $room->PAINT_NEEDS = $hasil;   
-        $room->save();        
-        return redirect()->route('bbs_calculate_paint_needs')
-            ->with('alert-success', 'Data Berhasil Disimpan.');
+        $room->save(); 
+            
+//        return redirect()->route('bbs_calculate_paint_needs')->with('alert-success', 'Data Berhasil Disimpan.');
     }
     
     public function input_act_bbs()
